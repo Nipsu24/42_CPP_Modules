4600d06cdbcd40064cbdc50a9571c98cb442032e
Marius Meier <marius.meier24@gmail.com>
2024-11-20 17:49:34 +0200

    adds initial structure for contact class

--- ex01/Contacts.hpp
+++ ex01/Contacts.hpp
@@ -1,1 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contacts.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmeier <mmeier@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/20 13:21:36 by mmeier            #+#    #+#             */
+/*   Updated: 2024/11/20 13:39:54 by mmeier           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#ifndef CONTACTS_HPP
+# define CONTACTS_HPP
+
+#include <cctype>
+
+class Contacts
+{
+private:
+	/* data */
+public:
+	std::string first_name;
+	std::string last_name;
+	std::string nickname;
+	std::string phone_no;
+	std::string darkest_secret;
+	
+	Contacts(/* args */);
+	~Contacts();
+};
+
+Contacts::Contacts(/* args */)
+{
+}
+
+Contacts::~Contacts()
+{
+}
+
+
+
+
+
+#endif
