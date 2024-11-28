bf3413846a891c5cb53c421fc8daf483b6810b78
Marius Meier <marius.meier24@gmail.com>
2024-11-28 15:31:07 +0200

    adds functionality with restriction (s1 and s2 have to be of same lenght)

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mmeier <mmeier@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 11:57:55 by mmeier            #+#    #+#             */
-/*   Updated: 2024/11/28 13:22:27 by mmeier           ###   ########.fr       */
+/*   Updated: 2024/11/28 15:30:05 by mmeier           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,45 +17,79 @@
 /*Prints out error message, in case argument amout given to program is not 3.*/
 int	printInvalidArguments()
 {
-	std::cout << "Error. Insufficient argument amount." << std::endl;
+	std::cout << "Error. Insufficient arguments." << std::endl;
 	std::cout << "Allowed argument amount is '3' in the following order:"<< std::endl;
 	std::cout << "filename, string1, string2." << std::endl;
+	std::cout << "string1 and string2 must be of the same length." << std::endl;
 	return (1);
 }
 
-std::ofstream	createSecondFile(char **av)
+/*Opens file or returns error in case it cannot be opened or is empty.
+  If file can be opened, reads its content and stores it in a string.*/
+int	createSecondFile(char **av, std::string fileContent)
+{
+	std::string		s1 = av[2];
+	std::string		s2 = av[3];
+
+	std::ofstream myFileReplace("file1.replace");
+	int j = fileContent.find(av[2], 0);
+	if ((const size_t) j == std::string::npos)
+	{
+		std::cout << "String '" << av[2] << "' was not found in file1.replace." << std::endl;
+		return (1);
+	}
+	for (size_t i = j, k = 0; i - j < s1.length(); i++, k++)
+		fileContent[i] = s2[k];
+	myFileReplace << fileContent;
+	myFileReplace.close();
+	return (0);
+}
+
+int	checkEqualArgumentLen(char **av)
+{
+	std::string	s1 = av[2];
+	std::string	s2 = av[3];
+	
+	if (s1.length() != s2.length())
+		return (1);
+	return (0);
+}
+
+std::string	getFileContent(char **av)
 {
 	std::ifstream	myFile;
 	std::string		fileContent;
 
 	myFile.open(av[1]);
-	if(!myFile.is_open())
+	if (!myFile.is_open())
 	{
 		std::cout << "Error. File 1 could not be opened." << std::endl;
-		return std::ofstream();
+		return ("");
 	}
 	if (!std::getline(myFile, fileContent))
 	{
 		std::cout << "Error. File 1 is empty." << std::endl;
-		return std::ofstream();
+		return ("");
 	}
 	else
 		myFile.close();
-	std::cout << "File 1 Content: " << fileContent << std::endl;
-	std::ofstream myFileReplace("file1.replace");
-	myFileReplace << fileContent;
-	myFileReplace.close();
-	return (myFileReplace);
+	return (fileContent);
 }
 
 int	main(int ac, char **av)
 {
+	std::string	fileContent;
+
 	if (ac != 4)
+		return (printInvalidArguments());
+	if (checkEqualArgumentLen(av))
 		return (printInvalidArguments());
 	else
 	{
-		std::ofstream myFileReplace = createSecondFile(av);
-		if (!myFileReplace)
+		fileContent = getFileContent(av);
+		if (fileContent.empty())
+			return (1);
+		if (createSecondFile(av, fileContent))
 			return (1);
 	}
 	return (0);
