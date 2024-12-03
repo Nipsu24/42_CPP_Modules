40aae3eb9d9521cd59dd83dbc63fb1911cb37ce7
Marius Meier <mmeier@student.hive.fi>
2024-12-03 14:56:02 +0200

    adds header and makefile

---
+++ cpp02/ex01/Fixed.hpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmeier <mmeier@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/29 09:55:43 by mmeier            #+#    #+#             */
+/*   Updated: 2024/12/03 14:55:23 by mmeier           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <iostream>
+
+class Fixed {
+	private:
+		int					mValue;
+		static const int	mFractionalBitsAmount = 8;
+	
+	public:
+		//Constructor
+		Fixed();
+		//Copy constructor
+		/*needed e.g. for "deep copies" of pointers
+		  (copying content the pointer is pointing to and not the pointer
+		  itself as this would end up with two objects calling delete on same
+		  non-null pointer and thus to heap corruption)*/
+		Fixed(const Fixed& other);
+		//Copy assignment operator
+		Fixed& operator=(const Fixed& other);
+		//Destructor
+		~Fixed();
+		
+		//Methods:
+		int		getRawBits(void) const;
+		void	setRawBits(int const raw);	
+		
+};
+
+#endif
