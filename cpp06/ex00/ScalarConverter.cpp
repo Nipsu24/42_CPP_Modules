0d762b85bdac1bcf0d62ddeb208b3b1bcef7bd30
Marius Meier <mmeier@student.hive.fi>
2025-02-05 11:11:31 +0200

    separates handling of float and double, cuts output of float and double by 1 digit after '.', handles pseudo literals outside of double function, main changed so that it excepts an argument passed to the program

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -6,13 +6,14 @@
 /*   By: mmeier <mmeier@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 12:40:57 by mmeier            #+#    #+#             */
-/*   Updated: 2025/02/04 16:57:48 by mmeier           ###   ########.fr       */
+/*   Updated: 2025/02/05 11:07:54 by mmeier           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 #include <limits>
 #include <string>
+#include <iomanip>
 
 ScalarConverter::ScalarConverter() {}
 
@@ -28,7 +29,7 @@ ScalarConverter& ScalarConverter::operator=(const ScalarConverter& other) {
 
 ScalarConverter:: ~ScalarConverter() {}
 
-
+/*Checks if literal consists of one character and if this is printable. Then cast it to the respective datatypes*/
 bool	handleChar(const std::string& literal) {
 	if (literal.length() == 1 && std::isalpha(literal[0]) && std::isprint(literal[0])) {
 		char c = literal[0];
@@ -41,24 +42,7 @@ bool	handleChar(const std::string& literal) {
 	return (false);
 }
 
-void	handleInt(const std::string& literal) {
-	try {
-		int nbr = std::stoi(literal);
-		char c = nbr;
-		if (!std::isprint(c)) {
-			std::cout << "char: Non displayable" << std::endl;
-		}
-		else
-			std::cout << "char: " << c << std::endl;
-		std::cout << "int: " << nbr << std::endl;
-		std::cout << "float: " << static_cast<float>(nbr) << ".0f" << std::endl;
-		std::cout << "double: " << static_cast<double>(nbr) << ".0" << std::endl;
-	}
-    catch (const std::out_of_range& e) {
-        std::cout << "int: not possible "  << e.what() << std::endl;
-    }
-}
-
+/*Hanldes the respective pseudo literals.*/
 bool	handlePseudoLiterals(const std::string& literal) {
 	if (literal == "nan" || literal == "nanf") {
 		std::cout << "char: impossible" << std::endl;
@@ -74,7 +58,8 @@ bool	handlePseudoLiterals(const std::string& literal) {
 		std::cout << "double: -inf" << std::endl;
 		return (true);
 	}
-	else if (literal == "+inf" || literal == "+inff") {
+	else if (literal == "+inf" || literal == "+inff"
+			|| literal == "inf" || literal == "inff") {
 		std::cout << "char: impossible" << std::endl;
 		std::cout << "int: impossible" << std::endl;
 		std::cout << "float: +inff" << std:: endl;
@@ -84,14 +69,65 @@ bool	handlePseudoLiterals(const std::string& literal) {
 	return (false);
 }
 
-void	handleFloatsAndDoubles(const std::string& literal) {
+/*Converts literal to int with stoi and catches possible exceptions. Checks also, if the resulting
+  number is a printable ascii character.*/
+void	handleInt(const std::string& literal) {
 	try {
-		if (handlePseudoLiterals(literal))
-			return ;
+		int nbr = std::stoi(literal);
+		char c = nbr;
+		if (!std::isprint(c)) {
+			std::cout << "char: Non displayable" << std::endl;
+		}
+		else
+			std::cout << "char: " << c << std::endl;
+		std::cout << "int: " << nbr << std::endl;
+		std::cout << std::fixed << std::setprecision(1);
+		std::cout << "float: " << static_cast<float>(nbr) << "f" << std::endl;
+		std::cout << "double: " << static_cast<double>(nbr) << std::endl;
+	}
+    catch (const std::out_of_range& e) {
+        std::cout << "int: not possible "  << e.what() << std::endl;
+    }
+	catch (const std::invalid_argument& e) {
+		std:: cout << "int: not possible " << e.what() << std::endl;
+	}
+}
+
+/*Trims string by removing 'f', then converts it to float and casts it to the respective datatypes. Checks
+  if char is displayable character and if value is within int min/max. Possible exception resulting from
+  stof are caught. Digits for floats and doubles are set to 1 digit after '.'*/
+void	handleFloats(const std::string& literal) {
+	try {
 		std::string cleanLiteral = literal;
-		if (cleanLiteral.back() == 'f')
-			cleanLiteral.pop_back();
-		double value = std::stod(cleanLiteral);
+		cleanLiteral.pop_back();
+		float value = std::stof(cleanLiteral);
+		char c = static_cast<int>(value); //needed as double cannot be directly cast to char
+		if (!std::isprint(c)) {
+			std::cout << "char: Non displayable" << std::endl;
+		}
+		else
+			std::cout << "char: " << c << std::endl;
+		if (value > static_cast<float>(std::numeric_limits<int>::max()) || value < static_cast<float>(std::numeric_limits<int>::min()))
+			std::cout << "int: impossible" << std::endl;
+		else 
+			std::cout << "int: " << static_cast<int>(value) << std::endl;
+		std::cout << std::fixed << std::setprecision(1);
+		std::cout << "float: " << value << "f" << std::endl;
+		std::cout << "double: " << static_cast<double>(value) << std::endl;
+	}
+	catch (const std::exception& e) {
+		std::cerr << "float: not possible " << e.what() << std::endl;
+	}
+	catch (const std::invalid_argument& e) {
+		std:: cerr << "float: not possible " << e.what() << std::endl;
+	}
+}
+
+/*Converts literal to double, possible exceptions caught in try/catch statement. Digits for floats and doubles
+  are set to 1 digit after '.'*/
+void	handleDoubles(const std::string& literal) {
+	try {
+		double value = std::stod(literal);
 		char c = static_cast<int>(value); //needed as double cannot be directly cast to char
 		if (!std::isprint(c)) {
 			std::cout << "char: Non displayable" << std::endl;
@@ -102,25 +138,21 @@ void	handleFloatsAndDoubles(const std::string& literal) {
 			std::cout << "int: impossible" << std::endl;
 		else 
 			std::cout << "int: " << static_cast<int>(value) << std::endl;
-		std::cout << "float: " << std::to_string(static_cast<float>(value)) + "f" << std::endl;
-		std::cout << "double: " << std::to_string(value) << std::endl;
+		std::cout << std::fixed << std::setprecision(1);
+		std::cout << "float: " << static_cast<float>(value) << "f" << std::endl;
+		std::cout << "double: " << value << std::endl;
 	}
 	catch (const std::exception& e) {
 		std::cerr << "double: not possible " << e.what() << std::endl;
 	}
-}
-
-bool isNumeric(const std::string& literal) {
-	size_t	j = 0;
-	if (literal[0] == '-')
-		j++;
-	for (size_t i = j; i < literal.length(); i++) {
-		if (!std::isdigit(literal[i]))
-			return (false);
+	catch (const std::invalid_argument& e) {
+		std:: cerr << "double: not possible " << e.what() << std::endl;
 	}
-	return (true);
 }
 
+/*Checks first for empty strings, then checks if string only consists of one char. Afterwards
+  checks the pseudo literals. Int is identified by abscence of '.', float by 'f' at end of string,
+  otherwise it is assumed that argument is a double.*/
 void	ScalarConverter::convert(const std::string& literal) {
 	if (literal.empty()) {
 		std::cout << "String passed to function is empty." << std::endl;
@@ -128,10 +160,15 @@ void	ScalarConverter::convert(const std::string& literal) {
 	}
 	if (handleChar(literal))
 		return ;
-	else if (isNumeric(literal)) {
+	if (handlePseudoLiterals(literal))
+		return ;
+	else if (literal.find('.') == std::string::npos) {
+	// else if (isNumeric(literal)) {
 		handleInt(literal);
 	}
-	else {
-		handleFloatsAndDoubles(literal);
+	else if (literal.back() == 'f') {
+		handleFloats(literal);
 	}
+	else
+		handleDoubles(literal);
 }
--- cpp06/ex00/main.cpp
+++ cpp06/ex00/main.cpp
@@ -6,53 +6,67 @@
 /*   By: mmeier <mmeier@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 15:25:43 by mmeier            #+#    #+#             */
-/*   Updated: 2025/02/04 16:51:30 by mmeier           ###   ########.fr       */
+/*   Updated: 2025/02/05 11:03:04 by mmeier           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 
-int	main(void) {
-	
-	std::cout << "a" << std::endl;
-	ScalarConverter::convert("a");
-	std::cout << std::endl;
-	
-	std::cout << "10.0f" << std::endl;
-	ScalarConverter::convert("10.0f");
-	std::cout << std::endl;
-	
-	std::cout << "42.0f" << std::endl;
-	ScalarConverter::convert("42.0f");
-	std::cout << std::endl;
-	
-	std::cout << "10" << std::endl;
-	ScalarConverter::convert("10");
-	std::cout << std::endl;
-	
-	std::cout << "2147483648" << std::endl;
-	ScalarConverter::convert("2147483648");
-	std::cout << std::endl;
-	
-	std::cout << "-2147483649" << std::endl;
-	ScalarConverter::convert("-2147483649");
-	std::cout << std::endl;
-	
-	std::cout << "nan" << std::endl;
-	ScalarConverter::convert("nan");
-	std::cout << std::endl;
-
-	std::cout << "--10" << std::endl;
-	ScalarConverter::convert("--10");
-	std::cout << std::endl;
-
-	std::cout << "-inf" << std::endl;
-	ScalarConverter::convert("-inf");
-	std::cout << std::endl;
-
-	std::cout << "+inff" << std::endl;
-	ScalarConverter::convert("+inff");
-	std::cout << std::endl;
-	
-	
+int	main(int ac, char *av[]) {
+	if (ac != 2) {
+		std::cout << "Insufficient arguments passed to program" << std::endl;
+		return (1);
+	}
+	std::string literal = av[1];
+	ScalarConverter::convert(literal);
+	return (0);
 }
+
+
+// int	main(void) {
+	
+// 	std::cout << "a" << std::endl;
+// 	ScalarConverter::convert("a");
+// 	std::cout << std::endl;
+	 
+// 	std::cout << "10.0f" << std::endl;
+// 	ScalarConverter::convert("10.0f");
+// 	std::cout << std::endl;
+	
+// 	std::cout << "42.0f" << std::endl;
+// 	ScalarConverter::convert("42.0f");
+// 	std::cout << std::endl;
+	
+// 	std::cout << "10" << std::endl;
+// 	ScalarConverter::convert("10");
+// 	std::cout << std::endl;
+	
+// 	std::cout << "2147483648" << std::endl;
+// 	ScalarConverter::convert("2147483648");
+// 	std::cout << std::endl;
+	
+// 	std::cout << "-2147483649" << std::endl;
+// 	ScalarConverter::convert("-2147483649");
+// 	std::cout << std::endl;
+	
+// 	std::cout << "nan" << std::endl;
+// 	ScalarConverter::convert("nan");
+// 	std::cout << std::endl;
+
+// 	std::cout << "--10" << std::endl;
+// 	ScalarConverter::convert("--10");
+// 	std::cout << std::endl;
+
+// 	std::cout << "-inf" << std::endl;
+// 	ScalarConverter::convert("-inf");
+// 	std::cout << std::endl;
+
+// 	std::cout << "+inff" << std::endl;
+// 	ScalarConverter::convert("+inff");
+// 	std::cout << std::endl;
+
+// 	std::cout << "23434.34" << std::endl;
+// 	ScalarConverter::convert("23434.34");
+// 	std::cout << std::endl;
+	
+// }
