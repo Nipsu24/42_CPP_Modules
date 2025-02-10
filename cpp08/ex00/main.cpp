b22f29baa71714e9a69071a643e5b68c0ee9e67e
Marius Meier <mmeier@student.hive.fi>
2025-02-10 15:06:16 +0200

    updates main and includes double test

--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mmeier <mmeier@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 15:25:43 by mmeier            #+#    #+#             */
-/*   Updated: 2025/02/10 14:13:52 by mmeier           ###   ########.fr       */
+/*   Updated: 2025/02/10 15:05:27 by mmeier           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,20 @@
 
 int main( void ) {
 	try {
-		std::cout << "Pass int 4 to array from 1 to 6:" << std::endl;
+		std::cout << "Pass int 4 to int array from 1 to 6:" << std::endl;
 		std::vector<int> intArray = {1, 2, 3, 4, 5, 6};
 		::easyfind(intArray, 4);
 		std::cout << "Integer appears in array." << std::endl;
+		
+		std::cout << "Pass int 4 to double array from 1.0 to 6.0:" << std::endl;
+		std::vector<double> doubleArray = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0};
+		::easyfind(doubleArray, 4);
+		std::cout << "Integer appears in array." << std::endl;
 
-		std::cout << "Pass int 7 to array from 1 to 6:" << std::endl;
+		std::cout << "Pass int 7 to int array from 1 to 6:" << std::endl;
 		::easyfind(intArray, 7);
 		std::cout << "Integer appears in array." << std::endl;
+
 	}
 	catch (std::exception& e) {
 		std::cerr << e.what() << std::endl;
