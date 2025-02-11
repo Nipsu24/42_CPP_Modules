6d9a42fbca7a8f7c5bd5b586c8d56909396720dc
Marius Meier <mmeier@student.hive.fi>
2025-02-11 16:00:40 +0200

    adds draft of class

---
+++ cpp08/ex02/MutantStack.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmeier <mmeier@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/06 13:48:35 by mmeier            #+#    #+#             */
+/*   Updated: 2025/02/11 15:59:47 by mmeier           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+#include <stack>
+
+template<class T>
+class MutantStack : public std::stack<T&> {
+	private:
+
+	public:
+		MutantStack();
+		MutantStack(const MutantStack& other);
+		MutantStack& operator=(const MutantStack& other);
+		~MutantStack();
+
+};
+
+#endif
